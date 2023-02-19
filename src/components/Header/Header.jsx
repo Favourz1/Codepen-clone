60ec1c5612f0f83152a22d77054780eb6e29265d
Favour Okoh <okohfavour91@gmail.com>
2023-02-19 08:16:34 +0100

    Chore: Added theme toggle icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import HeaderStyles from "./Header.module.css"
 import { themeContext, setThemeContext } from "../../contexts/ThemeContext"
 import FavourzLogoWhite from "../../assets/img/favourz-logo-white.png"
 import FavourzLogoBlack from "../../assets/img/favourz-logo-black.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+
 
 const Header = () => {
 
@@ -20,7 +23,7 @@ const Header = () => {
       <div className={HeaderStyles["header-action-wrapper"]}>
         <div className={`${HeaderStyles["header-action-btn"]} ${HeaderStyles["theme-btn"]}`}
           onClick={() => setDarkThemeContext(prevTheme => !prevTheme)}
-        >Toogle Theme</div>
+        > <FontAwesomeIcon icon={darkThemeContext ? faSun : faMoon} transform="right-4" size="md" /></div>
         <div className="header-action-btn">Change Editor Position</div>
       </div>
     </header>
