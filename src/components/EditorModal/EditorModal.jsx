36e6617e1f99dc52ce94b839d8c42ed6a948038a
Favour Okoh <okohfavour91@gmail.com>
2023-03-06 18:20:26 +0100

    Chore: Basic structure for editor position modal

---
+++ src/components/EditorModal/EditorModal.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import EditorLeft from '../Icons/EditorLeft'
+import EditorTop from '../Icons/EditorTop'
+import EditorRight from '../Icons/EditorRight'
+import { themeContext } from '../../contexts/ThemeContext'
+
+const EditorModal = ({ onblur }) => {
+    const darkThemeContext = useContext(themeContext);
+    return (
+        <div
+            className='editor-position__modal-container'
+            // onBlur={onblur}
+            // onMouseOut={onblur}
+            onClick={(el) => { el.target !== el ? onblur : undefined }}
+            //useRef here 👆
+            style={{
+                backgroundColor: darkThemeContext ? 'hsl(225, 6%, 25%)' : 'white',
+                color: darkThemeContext ? 'white' : 'hsl(225, 6%, 25%)',
+            }}
+        >
+            <div className='editor-position__modal-wrapper'>
+                <h3 className='editor-position__title'>Change View</h3>
+                <ul className='editor-position__action-wrapper'>
+                    <li><EditorLeft dark={darkThemeContext} /></li>
+                    <li><EditorTop dark={darkThemeContext} /></li>
+                    <li><EditorRight dark={darkThemeContext} /></li>
+                </ul>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default EditorModal
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import HeaderStyles from "./Header.module.css"
 import { themeContext, setThemeContext } from "../../contexts/ThemeContext"
 import FavourzLogoWhite from "../../assets/img/favourz-logo-white.png"
@@ -6,9 +6,12 @@ import FavourzLogoBlack from "../../assets/img/favourz-logo-black.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import EditorTop from "../Icons/EditorTop";
+import EditorModal from "../EditorModal/EditorModal";
 
 
 const Header = () => {
+
+  const [isEditorModalActive, setIsEditorModalActive] = useState(false)
 
   const darkThemeContext = useContext(themeContext)
   const setDarkThemeContext = useContext(setThemeContext)
@@ -18,7 +21,15 @@ const Header = () => {
     color: darkThemeContext ? 'white' : 'hsl(225, 6%, 25%)',
     fill: darkThemeContext ? 'white' : 'hsl(225, 6%, 25%)'
   }
+  //   const handleClick = (el) => {
+  //     setIsEditorModalActive(!isEditorModalActive)
+  //     if (el.target.contains({< EditorModal />})) {
 
+  //     }
+  //       else {
+  //       setIsEditorModalActive(false)
+  // }
+  //   }
   return (
     <header className={HeaderStyles.header} style={headerConditionalStyle} >
       <div className={`${HeaderStyles["logo-wrapper"]}`}>
@@ -29,9 +40,9 @@ const Header = () => {
           onClick={() => setDarkThemeContext(prevTheme => !prevTheme)}
           tabIndex={0}
         > <FontAwesomeIcon icon={darkThemeContext ? faSun : faMoon} transform="right-4" size="1x" /></div>
-        <div className={`${HeaderStyles["header-action-btn"]} ${HeaderStyles[""]}`}
-        >
+        <div className={`${HeaderStyles["header-action-btn"]} ${HeaderStyles[""]}`} onClick={() => setIsEditorModalActive(!isEditorModalActive)}>
           <EditorTop dark={darkThemeContext} />
+          {isEditorModalActive && <EditorModal onblur={() => setIsEditorModalActive(false)} />}
         </div>
       </div>
     </header>
