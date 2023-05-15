dfc9b7a5b7ec0052462999c7c9c388ec7ae07322
Favour Okoh <okohfavour91@gmail.com>
2023-05-15 07:05:55 +0100

    Chore: Created and styled info button on the header component

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,56 +1,65 @@
-import React, { useState, useContext, useRef } from 'react'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
-import 'codemirror/theme/base16-light.css'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/css/css'
-import 'codemirror/addon/display/fullscreen.js'
-import 'codemirror/addon/display/fullscreen.css'
-import editorStyles from './Editor.module.css'
-import { Controlled as ControlledEditor } from 'react-codemirror2'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
-import { themeContext } from "../../contexts/ThemeContext"
+import React, { useState, useContext } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/theme/base16-light.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import "codemirror/addon/display/fullscreen.js";
+import "codemirror/addon/display/fullscreen.css";
+import editorStyles from "./Editor.module.css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
+import { themeContext } from "../../contexts/ThemeContext";
 
-export default function Editor({ displayName, language, value, onChange, setWelcomeText, editorPosition }) {
+export default function Editor({
+  displayName,
+  language,
+  value,
+  onChange,
+  setWelcomeText,
+  editorPosition,
+}) {
+  const [open, setOpen] = useState(true);
 
-  const [open, setOpen] = useState(true)
-
-
-  const darkTheme = useContext(themeContext)
+  const darkTheme = useContext(themeContext);
 
   function handleChange(editor, data, value) {
-    setWelcomeText(false)
-    onChange(value)
+    setWelcomeText(false);
+    onChange(value);
   }
 
   const editorConditionalStyles = {
-    "top": {
-      editorContainer: { width: "calc(100% / 3)", height: "" }
+    top: {
+      editorContainer: { width: "calc(100% / 3)", height: "" },
     },
-    "left": {
-      editorContainer: { width: "100%", height: "calc(90% / 3)" }
+    left: {
+      editorContainer: { width: "100%", height: "calc(90% / 3)" },
     },
-    "right": {
-      editorContainer: { width: "100%", height: "calc(90% / 3)" }
-    }
-  }
+    right: {
+      editorContainer: { width: "100%", height: "calc(90% / 3)" },
+    },
+  };
 
   return (
-    <div className={`editor-container ${open ? '' : 'collapsed'} ${editorPosition !== "top" && !open ? editorStyles['collapse'] : ''}`} style={{
-      backgroundColor: darkTheme ? 'hsl(225, 6%, 25%)' : 'hsl(0deg 3% 73%)',
-      color: darkTheme ? 'white' : 'hsl(225, 6%, 25%)', ...editorConditionalStyles[editorPosition].editorContainer
-    }}>
+    <div
+      className={`editor-container ${open ? "" : "collapsed"} ${
+        editorPosition !== "top" && !open ? editorStyles["collapse"] : ""
+      }`}
+      style={{
+        backgroundColor: darkTheme ? "hsl(225, 6%, 25%)" : "hsl(0deg 3% 73%)",
+        color: darkTheme ? "white" : "hsl(225, 6%, 25%)",
+        ...editorConditionalStyles[editorPosition].editorContainer,
+      }}
+    >
       <div className="editor-title">
         {displayName}
         <button
-          className={editorStyles['expand-collapse-button']}
-          onClick={() => setOpen(prevOpen => !prevOpen)}
+          className={editorStyles["expand-collapse-button"]}
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
         >
-          <FontAwesomeIcon
-            icon={open ? faCompressAlt : faExpandAlt}
-          />
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
         </button>
       </div>
       <ControlledEditor
@@ -63,17 +72,17 @@ export default function Editor({ displayName, language, value, onChange, setWelc
           mode: language,
           lineNumbers: true,
           styleActiveLine: true,
-          theme: darkTheme ? 'material' : 'base16-light',
+          theme: darkTheme ? "material" : "base16-light",
           extraKeys: {
-            "F11": function (cm) {
+            F11: function (cm) {
               cm.setOption("fullScreen", !cm.getOption("fullScreen"));
             },
-            "Esc": function (cm) {
+            Esc: function (cm) {
               if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
-            }
-          }
+            },
+          },
         }}
       />
     </div>
-  )
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,67 +1,121 @@
 import React, { useContext, useState, useEffect } from "react";
-import HeaderStyles from "./Header.module.css"
-import { themeContext, setThemeContext } from "../../contexts/ThemeContext"
-import FavourzLogoWhite from "../../assets/img/favourz-logo-white.png"
-import FavourzLogoBlack from "../../assets/img/favourz-logo-black.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import HeaderStyles from "./Header.module.css";
+import { themeContext, setThemeContext } from "../../contexts/ThemeContext";
+import FavourzLogoWhite from "../../assets/img/favourz-logo-white.png";
+import FavourzLogoBlack from "../../assets/img/favourz-logo-black.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faInfo } from "@fortawesome/free-solid-svg-icons";
 import EditorTop from "../Icons/EditorTop";
 import EditorLeft from "../Icons/EditorLeft";
 import EditorRight from "../Icons/EditorRight";
 import EditorModal from "../EditorModal/EditorModal";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
+const Header = ({
+  editorPosition = "top",
+  changeEditorPosition,
+  changeAbstractDarkTheme,
+}) => {
+  const [isEditorModalActive, setIsEditorModalActive] = useState(false);
 
-const Header = ({ editorPosition = "top", changeEditorPosition, changeAbstractDarkTheme }) => {
-
-  const [isEditorModalActive, setIsEditorModalActive] = useState(false)
-
-  const darkThemeContext = useContext(themeContext)
-  const setDarkThemeContext = useContext(setThemeContext)
+  const darkThemeContext = useContext(themeContext);
+  const setDarkThemeContext = useContext(setThemeContext);
 
   const setEditorIcon = {
-    "top": () => <EditorTop dark={darkThemeContext} />,
-    "left": () => <EditorLeft dark={darkThemeContext} />,
-    "right": () => <EditorRight dark={darkThemeContext} />
-  }
+    top: () => <EditorTop dark={darkThemeContext} />,
+    left: () => <EditorLeft dark={darkThemeContext} />,
+    right: () => <EditorRight dark={darkThemeContext} />,
+  };
 
   const headerConditionalStyle = {
-    backgroundColor: darkThemeContext ? 'hsl(225, 6%, 25%)' : 'white',
-    color: darkThemeContext ? 'white' : 'hsl(225, 6%, 25%)',
-    fill: darkThemeContext ? 'white' : 'hsl(225, 6%, 25%)'
-  }
+    backgroundColor: darkThemeContext ? "hsl(225, 6%, 25%)" : "white",
+    color: darkThemeContext ? "white" : "hsl(225, 6%, 25%)",
+    fill: darkThemeContext ? "white" : "hsl(225, 6%, 25%)",
+  };
 
   useEffect(() => {
-    changeAbstractDarkTheme(darkThemeContext)
-  }, [darkThemeContext])
+    changeAbstractDarkTheme(darkThemeContext);
+  }, [darkThemeContext]);
   return (
-    <header className={HeaderStyles.header} style={headerConditionalStyle} >
+    <header className={HeaderStyles.header} style={headerConditionalStyle}>
       <div className={`${HeaderStyles["logo-wrapper"]}`}>
-        <img src={darkThemeContext ? FavourzLogoWhite : FavourzLogoBlack} alt="Favour Okoh Logo" />
+        <img
+          src={darkThemeContext ? FavourzLogoWhite : FavourzLogoBlack}
+          alt="Favour Okoh Logo"
+        />
       </div>
       <div className={HeaderStyles["header-action-wrapper"]}>
-        <div className={`${HeaderStyles["header-action-btn"]} ${HeaderStyles["theme-btn"]}`}
-          onClick={() => setDarkThemeContext(prevTheme => !prevTheme)}
+        <div
+          className={`${HeaderStyles["header-action-btn"]} ${HeaderStyles["info-container"]}`}
+        >
+          <FontAwesomeIcon
+            icon={faInfo}
+            className={HeaderStyles["info-icon"]}
+          />
+          <div
+            className={HeaderStyles["info-content-wrapper"]}
+            style={headerConditionalStyle}
+          >
+            <div className={HeaderStyles["info-content-header"]}>INFO:</div>
+            <ul className={HeaderStyles["info-content-list-wrapper"]}>
+              <li className={HeaderStyles["info-content-list-item"]}>
+                Enter fullscreen mode
+              </li>
+              <li className={HeaderStyles["info-content-list-item"]}>
+                Exit fullscreen mode
+              </li>
+              <li className={HeaderStyles["info-content-list-item"]}>
+                Display console terminal (bottom left)
+              </li>
+              <li className={HeaderStyles["info-content-list-item"]}>
+                Love this? My{" "}
+                <a
+                  href="https://www.linkedin.com/in/favour-okoh/"
+                  target="_blank"
+                >
+                  Portfolio
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div
+          className={`${HeaderStyles["header-action-btn"]} ${HeaderStyles["theme-btn"]}`}
+          onClick={() => setDarkThemeContext((prevTheme) => !prevTheme)}
           tabIndex={0}
-        > <FontAwesomeIcon icon={darkThemeContext ? faSun : faMoon} transform="right-4" /></div>
-        <div className={`${HeaderStyles["header-action-btn"]} ${HeaderStyles[""]}`} onClick={() => setIsEditorModalActive(!isEditorModalActive)}>
+        >
+          {" "}
+          <FontAwesomeIcon
+            icon={darkThemeContext ? faSun : faMoon}
+            transform="right-4"
+          />
+        </div>
+        <div
+          className={`${HeaderStyles["header-action-btn"]} ${HeaderStyles[""]}`}
+          onClick={() => setIsEditorModalActive(!isEditorModalActive)}
+        >
           {setEditorIcon[editorPosition]()}
-          {isEditorModalActive && <EditorModal editorPosition={editorPosition} changeEditorPosition={changeEditorPosition} onblur={() => setIsEditorModalActive(false)} />}
+          {isEditorModalActive && (
+            <EditorModal
+              editorPosition={editorPosition}
+              changeEditorPosition={changeEditorPosition}
+              onblur={() => setIsEditorModalActive(false)}
+            />
+          )}
         </div>
       </div>
     </header>
   );
-
-
 };
 
 Header.propTypes = {
   editorPosition: PropTypes.string.isRequired,
-  changeEditorPosition: PropTypes.func
-}
+  changeEditorPosition: PropTypes.func,
+};
 
 Header.defaultProps = {
-  editorPosition: "top"
-}
+  editorPosition: "top",
+};
 
 export default Header;
